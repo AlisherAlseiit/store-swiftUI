97b4049c32f00e226c7d3f3d427ed2bfd535362f
Alisher <Alisher>
2021-10-24 12:35:40 +0600

    Initial Commit

---
+++ Shared/store_swiftUI_seserviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  store_swiftUI_seserviceApp.swift
+//  Shared
+//
+//  Created by Алишер Алсейт on 24.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct store_swiftUI_seserviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
